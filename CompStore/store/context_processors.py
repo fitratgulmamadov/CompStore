dab10400d59e6aa912f38fc5415c8a6446a3d400
Fitrat Gulmamadov <fitratgulmamadov7@gmail.com>
2026-03-29 02:55:22 +0500

    Move SITE_NAME to settings, fix .gitignore for staticfiles

--- CompStore/store/context_processors.py
+++ CompStore/store/context_processors.py
@@ -1,12 +1,10 @@
+from django.conf import settings
+
 from .models import Cart
-# from admin_interface.models import Theme
 
 
 def cart_context(request):
     cart = None
     if request.session.session_key:
         cart = Cart.objects.filter(session_key=request.session.session_key).first()
-    # theme = Theme.objects.get_active()
-    # site_name = theme.title if theme else 'CompStore'
-    site_name = 'CompStore'  # Фиксированное название сайта
-    return {'cart': cart, 'site_name': site_name}
+    return {'cart': cart, 'site_name': settings.SITE_NAME}
